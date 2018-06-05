0f843779e8cffa893145f085221f586fa4dce45e
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-05 18:43:08 +0300

    Router-link and save history

--- src/router.js
+++ src/router.js
@@ -7,6 +7,8 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
+  linkActiveClass: 'is-active',
+  scrollBehavior: (to, from, savedPosition) => ({ y: 0 }),
   routes: [
     { path: '/login', component: Login },
     { path: '/', component: Category }
--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav class="nav has-shadow">
-    <div class="container">
-      <a href="/">
+  <nav class="navbar has-shadow" role="navigation">
+    <div class="navbar-brand">
+      <router-link to="/" exact>
         <img src="http://bit.ly/vue-img" alt="Vue SPA" />
-      </a>
+      </router-link>
+      <router-link class="navbar-item is-tab" to="/" exact>Home</router-link>
+      <router-link class="navbar-item is-tab" to="/login">Login</router-link>
     </div>
   </nav>
 </template>
