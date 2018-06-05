d1248d7dd6336bb2c75cd3e111919f205a841b70
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-05 16:21:14 +0300

    Slots

--- src/theme/Post.vue
+++ src/theme/Post.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="card">
     <div class="card-content">
-      <h3>{{ post.title }}</h3>
-      {{ post.content }}
+      <slot name="title"></slot>
+      <slot name="content"></slot>
     </div>
     <footer class="card-footer">
-      <a class="card-footer-item" :href="post.link" target="_blank">Read more</a>
+      <a class="card-footer-item" :href="link" target="_blank">Read more</a>
     </footer>
   </div>
 </template>
 <script type="text/javascript">
   export default {
-    props: ['post']
+    props: ['link']
   }
 </script>
