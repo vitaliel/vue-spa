fdfea972974c87d1d42e6cc1c3fcd208f152189a
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-05 12:51:10 +0300

    Hot replacement setup

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 
 const app = new Vue({
   data: {
-    hello: 'Hi there Cub'
-  }
+    hello: 'Hi there little Noob!!!'
+  },
+  template: '<div id="app">{{ hello }}</div>'
+
 })
 
 export { app };
