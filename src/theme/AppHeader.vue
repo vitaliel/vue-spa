de6f2838f9907e568af83c33ab07b1caeb4dfac9
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-05 18:48:09 +0300

    Router redirect

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="navbar has-shadow" role="navigation">
     <div class="navbar-brand">
-      <router-link to="/" exact>
+      <router-link to="/category/front-end" exact>
         <img src="http://bit.ly/vue-img" alt="Vue SPA" />
       </router-link>
-      <router-link class="navbar-item is-tab" to="/" exact>Home</router-link>
+      <router-link class="navbar-item is-tab" to="/category/front-end" exact>Home</router-link>
       <router-link class="navbar-item is-tab" to="/login">Login</router-link>
     </div>
   </nav>
