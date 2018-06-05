9fa47bc79a9bb9f4da582092581cc094c84846a0
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-05 19:20:45 +0300

    Named routes and wildcard route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Category from './theme/Category.vue'
 import Login from './theme/Login.vue'
+import NotFound from './theme/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -11,8 +12,9 @@ const router = new VueRouter({
   scrollBehavior: (to, from, savedPosition) => ({ y: 0 }),
   routes: [
     { path: '/login', component: Login },
-    { path: '/category/:id', component: Category },
-    { path: '/', redirect: '/category/front-end' }
+    { path: '/category/:id', name: 'category', component: Category },
+    { path: '/', redirect: '/category/front-end' },
+    { path: '*', component: NotFound }
   ]
 })
 
--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -4,8 +4,8 @@
       <router-link to="/category/front-end" exact>
         <img src="http://bit.ly/vue-img" alt="Vue SPA" />
       </router-link>
-      <router-link class="navbar-item is-tab" to="/category/front-end" exact>Front-end</router-link>
-            <router-link class="navbar-item is-tab" to="/category/mobile" exact>Mobile</router-link>
+      <router-link class="navbar-item is-tab" :to="{name: 'category', params: {id:'front-end'}}">Front-end</router-link>
+            <router-link class="navbar-item is-tab" to="/category/mobile">Mobile</router-link>
       <router-link class="navbar-item is-tab" to="/login">Login</router-link>
     </div>
   </nav>
