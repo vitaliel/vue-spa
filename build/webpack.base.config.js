f7a9801b19c1ac6a348229fb8efb4e7ed6f39d77
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-05 12:07:16 +0300

    Simple start

---
+++ build/webpack.base.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const config = {
+  mode: process.env.NODE_ENV || "development",
+  entry: {
+    app: path.resolve(__dirname, '../src/client-entry.js')
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: "/",
+    filename: "assets/js/[name].js"
+  }
+}
+
+module.exports = config;
