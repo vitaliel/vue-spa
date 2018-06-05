b8142abdd16ae43cce3ee4d019445529a086b70b
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-05 16:13:15 +0300

    Custom properties

---
+++ src/theme/Post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <h3>{{ post.title }}</h3>
+      {{ post.content }}
+    </div>
+    <footer class="card-footer">
+      <a class="card-footer-item" :href="post.link" target="_blank">Read more</a>
+    </footer>
+  </div>
+</template>
+<script type="text/javascript">
+  export default {
+    props: ['post']
+  }
+</script>
