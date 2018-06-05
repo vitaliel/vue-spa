664e569d0709ed87d6a8885199e906bc1988c102
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-05 18:22:40 +0300

    Added vue-router
    Login page

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Category from './theme/Category.vue'
+import Login from './theme/Login.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/login', component: Login },
+    { path: '/', component: Category }
+  ]
+})
+
+export default router
---
+++ src/theme/Login.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="content">
+  <h2>Login</h2>
+  <div class="field is-horizontal">
+    <div class="field-label is-normal">
+      <label class="label">Username</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <input type="text" class="input" placeholder="Your username">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end user field -->
+
+  <div class="field is-horizontal">
+    <div class="field-label is-normal">
+      <label class="label">Password</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <input type="password" class="input" placeholder="Your password">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end password -->
+  <div class="field is-horizontal">
+    <div class="field-label is-normal">
+      <!-- Left empty for spacing -->
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <button class="button is-primary">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+</template>
+<script type="text/javascript">
+
+</script>
